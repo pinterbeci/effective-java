3d2c6d3c4944106aacb8fb9920c8e0baf321c14d
pinterbeci <pinter.beci99@gmail.com>
2025-01-07 18:09:34 +0100

    rename Main -> Runner

--- src/Runner.java
+++ src/Runner.java
@@ -1,6 +1,6 @@
 import chapter1.item2.NutritionFacts;
 
-public class Main {
+public class Runner {
     public static void main(String[] args) {
 
         final NutritionFacts nutritionFacts = new NutritionFacts.Builder(10, 10).calories(55).fat(55).build();
